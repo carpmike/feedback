b3bbf574cd37cbc29aa1dc5f628b96c56c3dea22
Mike Carpenter <mcarpenter@summa-tech.com>
2013-12-01 20:30:35 -0500

    Getting the size correct on the device

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp', [
+    'ngTouch',
+    'ngRoute',
+    'ngAnimate',
+    'myApp.controllers',
+    'myApp.memoryServices'
+]).
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/receivers', {templateUrl: 'partials/employee-list.html', controller: 'EmployeeListCtrl'});
+    $routeProvider.when('/feedback/:employeeId', {templateUrl: 'partials/feedback-choose.html', controller: 'FeedbackChooseCtrl'});
+    $routeProvider.when('/categorize/:employeeId:positive', {templateUrl: 'partials/feedback-categories.html', controller: 'FeedbackCategorizeCtrl'});
+    $routeProvider.when('/notify/:employeeId:positive', {templateUrl: 'partials/feedback-notification.html', controller: 'FeedbackNotifierCtrl'});
+    $routeProvider.otherwise({redirectTo: '/receivers'});
+}]);
