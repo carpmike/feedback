f7e731a8ab371fe211ced99ef74fdc10d0459c80
Mike Carpenter <mcarpenter@summa-tech.com>
2014-03-23 11:17:30 -0400

    got authentication working

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,17 @@
-'use strict';
-
-var fbURL = 'http://feedback-web.carpmike.cloudbees.net';
-//var fbURL = 'http://localhost:8080/feedback-web'
+// var fbURL = 'http://feedback-web.carpmike.cloudbees.net';
+var fbURL = 'http://localhost:8080/feedback-web';
 
 angular.module('myApp', [
     'ngTouch',
     'ngRoute',
     'ngAnimate',
+    'http-auth-interceptor',
     'myApp.controllers',
     'myApp.restServices',
     'myApp.domainClasses'
 ]).
 config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
     $routeProvider.when('/people', {templateUrl: 'partials/people.html', controller: 'PeopleCtrl'});
     $routeProvider.when('/people/:personId/fbtype', {templateUrl: 'partials/feedback-type.html', controller: 'FeedbackTypeCtrl'});
     $routeProvider.when('/people/:personId/fbtype/:fbtypeId/fbcat', {templateUrl: 'partials/feedback-categories.html', controller: 'FeedbackCategoryCtrl'});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,38 @@
-'use strict';
-
 angular.module('myApp.controllers', ['ngResource'])
     .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
         $scope.slide = '';
         $rootScope.back = function() {
           $scope.slide = 'slide-right';
           $window.history.back();
-        }
+        };
         $rootScope.go = function(path){
           $scope.slide = 'slide-left';
           $location.url(path);
-        }
+        };
         $rootScope.home = function(){
         	$scope.slide = 'slide-right';
         	$location.url('/people');
-        }
+        };
+
+        // handle the authn error by forcing login - uses the auth.js modules
+        $rootScope.$on('event:auth-loginRequired', function(event, rejection) {
+            console.log("got the login required event with status: " + rejection.status);  
+            $scope.slide = 'slide-left';
+            $location.url('/login');
+        });
+
+    }])
+    .controller('LoginCtrl', ['$scope', '$location', '$http', 'authService', function ($scope, $location, $http, authService) {
+        var user = {"username":"", "password":""};
+        $scope.user = user;
+
+        $scope.login = function() {
+            var encodedUserNameAndPassword = window.btoa(user.username + ':' + user.password);
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedUserNameAndPassword;
+            console.log("auth string: " + $http.defaults.headers.common['Authorization']);
+            authService.loginConfirmed();
+            $location.url('/people');
+        };
     }])
     .controller('PeopleCtrl', ['$scope', 'people', function ($scope, people) {
         $scope.people = people.people;
